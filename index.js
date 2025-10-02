613fe9ff5a2847c9be6c208d46b5370d210492da
Sheetal Sharma <sheetalsharmaoct2003@gmail.com>
2025-10-02 14:45:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("SHEETAL SHARMA.... LNMIIT")
+    res.send("SHEETAL SHARMA.... LNMIIT@CSE")
 });
 
 app.listen(process.env.PORT, (req, res)=>{

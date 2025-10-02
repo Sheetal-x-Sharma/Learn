487db9ce2c4646378c931158a00ff9a8b55894fe
Your Name <you@example.com>
2025-10-02 13:08:02 +0530

    I am happy

---
+++ index.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("SHEETAL SHARMA.... LNMIIT")
+});
+
+app.listen(process.env.PORT, (req, res)=>{
+    console.log(`Hello Sheetal Sharma ${process.env.PORT}`)
+});
